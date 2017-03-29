0463f7c35286a15bff54671e813ccf66f7f1883f
ninaradakovic <ninasradakovic@gmail.com>
2017-03-29 22:46:14 +0200

    Dodate u klasu Valuta metode equals, hashCode i toString.

--- Menjacnica/src/Valuta.java
+++ Menjacnica/src/Valuta.java
@@ -73,6 +73,55 @@ public class Valuta
 		
 		throw new RuntimeException("Pogresan datum je unet.");
 	}
+
+	@Override
+	public String toString()
+	{
+		return "Valuta [naziv=" + naziv + ", skracenica=" + skracenica + ", kursevi=" + kursevi + "]";
+	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kursevi == null) ? 0 : kursevi.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skracenica == null) ? 0 : skracenica.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (kursevi == null)
+		{
+			if (other.kursevi != null)
+				return false;
+		} else if (!kursevi.equals(other.kursevi))
+			return false;
+		if (naziv == null)
+		{
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skracenica == null)
+		{
+			if (other.skracenica != null)
+				return false;
+		} else if (!skracenica.equals(other.skracenica))
+			return false;
+		return true;
+	}
+	
 	
 	
 }
